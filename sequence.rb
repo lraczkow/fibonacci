818db0e6a7485983fbbccacb1097d6688d3ab9e3
Lauren Raczkowsk <lracz117@gmail.com>
2019-09-02 19:52:39 +0000

    recursive solution

--- sequence.rb
+++ sequence.rb
@@ -1,12 +1,29 @@
-def fibonacci(n)
-  if n==0
-    0
-  elsif n==1
-    1
-  else
-    fibonacci(n-1) + fibonacci(n-2)
-  end
+# def fibonacci(n)
+#   if n==0
+#     0
+#   elsif n==1
+#     1
+#   else
+#     fibonacci(n-1) + fibonacci(n-2)
+#   end
+# end
+
+# n = 7
+# puts fibonacci(n)
+
+def recursive_fib(n)
+  num = [0, 1]
+
+  fib = ->(num) {
+    if num.size == n
+      num
+    else
+      cur, last = num.last(2)
+      fib.(num + [cur + last])
+    end
+  }
+
+  fib.(num)
 end
 
-n = 7
-puts fibonacci(n)
+puts recursive_fib(10) # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
