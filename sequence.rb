24f70f5ec88cdb4b4164105150fd7e8391fe199c
Lauren Raczkowsk <lracz117@gmail.com>
2019-09-03 22:41:35 +0000

    iterative solution

--- sequence.rb
+++ sequence.rb
@@ -11,19 +11,36 @@
 # n = 7
 # puts fibonacci(n)
 
-def recursive_fib(n)
-  num = [0, 1]
+# def recursive_fib(n)
+#   num = [0, 1]
 
-  fib = ->(num) {
-    if num.size == n
-      num
-    else
-      cur, last = num.last(2)
-      fib.(num + [cur + last])
-    end
-  }
+#   fib = ->(num) {
+#     if num.size == n
+#       num
+#     else
+#       cur, last = num.last(2)
+#       fib.(num + [cur + last])
+#     end
+#   }
 
-  fib.(num)
+#   fib.(num)
+# end
+
+# puts recursive_fib(10)
+
+def iterative_fib(n)
+
+  a = 0
+  b = 1
+
+  n.times do
+    fib = a
+    a = b
+    b = fib + b
+  end
+
+  return a
+
 end
 
-puts recursive_fib(10) # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+puts iterative_fib(9)
